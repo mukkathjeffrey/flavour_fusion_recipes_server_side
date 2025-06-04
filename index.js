51c1476ec715a8add1609c4d602cf4898f0e1ad1
mukkathjeffrey <jeffreymukkath65@gmail.com>
2025-06-04 20:05:19 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import "dotenv/config"; // loads environment variables from .env
+
+const app = express();
+// parses incoming json requests
+app.use(express.json());
+// parses url-encoded data (from forms etc)
+app.use(express.urlencoded({ extended: true }));
+const PORT = process.env.PORT || 5000; // sets port from env or defaults to 5000
+
+// root route
+app.get("/", (req, res) => {
+  res.send("HELLO, WORLD!");
+});
+
+// starts the server
+app.listen(PORT, () => {
+  console.log(`server is running on port ${PORT}`);
+});
