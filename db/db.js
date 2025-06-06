183a8ff8edd5b4c40b052af499536d3909ecc63f
mukkathjeffrey <jeffreymukkath65@gmail.com>
2025-06-06 23:21:18 +0530

    establish database connection in backend setup

---
+++ db/db.js
@@ -0,0 +1,35 @@
+import { MongoClient, ServerApiVersion } from "mongodb";
+
+const MONGO_URI = process.env.MONGO_URI;
+
+const client = new MongoClient(MONGO_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+let db = null;
+
+async function connect_db() {
+  if (db) {
+    console.log("db already available");
+    return db;
+  }
+
+  try {
+    await client.connect();
+    db = client.db("flavour_fusion_recipes");
+    await db.command({ ping: 1 });
+    console.log(
+      "pinged your deployment. you successfully connected to mongodb!"
+    );
+    return db;
+  } catch (error) {
+    console.error("failed to connect to mongodb:", error);
+    throw error;
+  }
+}
+
+export default connect_db;
--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import express from "express";
 import "dotenv/config"; // loads environment variables from .env
+import connect_db from "./db/db.js";
 
 const app = express();
 // parses incoming json requests
 app.use(express.json());
 // parses url-encoded data (from forms etc)
 app.use(express.urlencoded({ extended: true }));
+await connect_db();
 const PORT = process.env.PORT || 5000; // sets port from env or defaults to 5000
 
 // root route
